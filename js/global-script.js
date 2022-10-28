d8230c43c711c95e052cbeb6e7fd93bdcc322be6
Eduardo <eduhmq@gmail.com>
2022-10-28 01:56:17 -0300

    fun: fazer login

--- js/global-script.js
+++ js/global-script.js
@@ -14,45 +14,54 @@ Documentacao => https://github.com/typicode/json-server
 
 const url = 'http://localhost:3000'
 
-async function getUsers(){
-  const response = await fetch(`${url}/users`);
-  const data = response.json();
-  
- data.then(value => console.log(value));
-  
+
+let dataUsers;
+let dataVagas = [];
+let dataCandidaturas = [];
+
+
+async function getUsers() {
+    const response = await fetch(`${url}/users`);
+    dataUsers = await response.json();
+
+    // console.log(dataUsers);
+
 }
-async function getVagas(){
-  const response = await fetch(`${url}/vagas`);
-  const data = response.json();
-  
- data.then(value => console.log(value));
-  
+async function getVagas() {
+    const response = await fetch(`${url}/vagas`);
+    dataVagas = await response.json();
+
+    // console.log(dataVagas);
+
 }
-async function getCandidaturas(){
-  const response = await fetch(`${url}/candidatura`);
-  const data = response.json();
-  
- data.then(value => console.log(value));
-  
+async function getCandidaturas() {
+    const response = await fetch(`${url}/candidatura`);
+    dataCandidaturas = await response.json();
+
+
 }
 
-getUsers(); 
+getUsers();
 
 getVagas();
 
 getCandidaturas();
 
+async function fazerLogin() {
+    let emailLogin = document.getElementById('emailLogin').value;
+    let senhaLogin = document.getElementById('senhaLogin').value;
 
+    let tamanho = await dataUsers.length;
+    let emailsCadastrados = new Map();
+    let senhas = new Map();
+    for (let i = 0; i < tamanho; i++) {
+        emailsCadastrados.set(await dataUsers[i].email, i);
+        senhas.set(await dataUsers[i].email, await dataUsers[i].senha);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    if (emailsCadastrados.has(emailLogin)) {
+        if (senhas.get(emailLogin) == senhaLogin) {
+            document.getElementById('logado').innerText = 'LOGADO'
+        }
+    }
+}
