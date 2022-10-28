0eb0f05a289be38f6bd052fa995236ed987458c4
Alain <alaindelonm@gmail.com>
2022-10-28 09:02:55 -0300

    feat: funcao deletar vaga

--- js/global-script.js
+++ js/global-script.js
@@ -1,43 +1,77 @@
-const url = 'http://localhost:3000'
+const url = "http://localhost:3000";
 
-async function getUsers(){
+async function getUsers() {
   const response = await fetch(`${url}/users`);
   const data = await response.json();
-  
- console.log(data) 
+
+  console.log(data);
+
 }
-async function getVagas(){
+async function getVagas() {
   const response = await fetch(`${url}/vagas`);
-  const data = response.json();
-  
- data.then(value => console.log(value));
-  
+  const data = await response.json();
+
+  console.log(data);
 }
-async function getCandidaturas(){
+async function getCandidaturas() {
   const response = await fetch(`${url}/candidatura`);
   const data = await response.json();
-  
-console.log(data);
-  
+
+  console.log(data);
 }
 
-getUsers(); 
+getUsers();
 
 getVagas();
 
 getCandidaturas();
 
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  postSignup(event);
+});
 
+function postSignup(e) {
+  let data = new FormData(e.target).entries();
+  let userSignup = Object.fromEntries(data);
 
+  try {
+    if(userSignup.nome === '' || userSignup.email === '' || userSignup.senha === ''){ 
+      throw new Error("campos vazios")
+    };
 
+    const json = {
+      tipo: "userSignup.tipo",
+      nome: "userSignup.nome",
+      dataNascimento: "userSignup.dataNascimento",
+      email: "userSignup.email",
+      senha: "userSignup.senha",
+    };
+    
+    fetch(`${url}/users`, {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify(json),
+     });
+  
+  } catch (error) {
+    console.error(error)
+  }
 
+}
 
+// FORMATA A DATA DE NASCIMENTO
+//let data = document.getElementById('input-signup-date').value;
+//let dataObj = new Date(data)
+//let dataObjNoTimeZone = new Date(dataObj.getTime() + ((dataObj.getTimezoneOffset() * 60000)))
 
-
-
-
-
-
-
-
-
+async function deleteVaga(id) {
+    await fetch(`${url}/vagas/${id}`, {
+        method: "DELETE",
+        headers: {
+        "Content-Type": "application/json",
+      },
+    });
+}
